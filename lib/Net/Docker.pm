0ee7b4af9dd1f909bdd0214c9ee18b68ddc35993
Peter Stuifzand <peter@stuifzand.eu>
2013-08-05 18:03:15 +0200

    Add start and streaming_logs method

--- lib/Net/Docker.pm
+++ lib/Net/Docker.pm
@@ -8,6 +8,9 @@ use URI;
 use URI::QueryParam;
 use LWP::UserAgent;
 use Carp;
+use AnyEvent;
+use AnyEvent::HTTP;
+use Data::Dumper;
 
 has address => (is => 'ro', default => 'http://127.0.0.1:4243');
 has ua      => (is => 'lazy');
@@ -17,7 +20,9 @@ sub _build_ua {
     return $ua;
 }
 
-sub _uri {
+sub _uri { my $self = shift; return $self->uri(@_); }
+
+sub uri {
     my ($self, $rel, %options) = @_;
     my $uri = URI->new($self->address . $rel);
     $uri->query_form(%options);
@@ -38,14 +43,28 @@ sub _parse {
 sub _parse_request {
     my ($self, $res) = @_;
     if ($res->content_type eq 'application/json') {
-        #my @lines = split/\n/, $res->decoded_content;
-        #return map { decode_json($_) } @lines;
         my $json = JSON::XS->new;
         return $json->incr_parse($res->decoded_content);
     }
     my $message = $res->decoded_content;
     $message =~ s/\r?\n$//;
     croak $message;
+}
+
+sub create {
+    my ($self, %options) = @_;
+    $options{AttachStderr} //= \1;
+    $options{AttachStdout} //= \1;
+    $options{AttachStdin}  //= \0;
+    $options{OpenStdin}  //= \0;
+    $options{Tty} = \1;
+    my $input = encode_json(\%options);
+
+    my $res = $self->ua->post($self->uri('/containers/create'), 'Content-Type' => 'application/json', Content => $input);
+
+    my $json = JSON::XS->new;
+    my $out = $json->incr_parse($res->decoded_content);
+    return $out->{Id};
 }
 
 sub ps {
@@ -150,13 +169,44 @@ sub stop {
 sub logs {
     my ($self, $container) = @_;
     my %params = (
-        logs => 1,
+        logs   => 1,
         stdout => 1,
         stderr => 1,
     );
     my $url = $self->_uri('/containers/'.$container.'/attach');
     my $res = $self->ua->post($url, \%params);
     return $res->content;
+}
+
+sub streaming_logs {
+    my $cb = pop @_; # last argument is the callback
+
+    my ($self, $container, %options) = @_;
+
+    if (ref($cb) ne 'CODE') {
+        croak "Callback is not a code ref";
+    }
+
+    my $uri = $self->uri('/containers/'.$container.'/attach', %options);
+
+    my $cv = AnyEvent->condvar;
+
+    my $callback; $callback = sub {
+        my ($data, $headers) = @_;
+        $cv->send;
+    };
+
+    my %get_opt = (
+        on_body => sub {
+            my ($partial, $headers) = @_;
+            $cb->($partial);
+            return 1;
+        },
+    );
+
+    http_request(POST => $uri->as_string, %get_opt, $callback);
+
+    return $cv;
 }
 
 1;
